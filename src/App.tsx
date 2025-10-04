9fa119ce41c405fa5c10249bb06bf19dc7a8b5b6
y0hannes <yohanneset3@gmail.com>
2025-10-04 11:45:41 +0300

    Add clear all functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import { useState } from 'react'
-import { marked } from 'marked'
-import './App.css'
+import { useState } from 'react';
+import { marked } from 'marked';
+import './App.css';
 
 const App: React.FC = () => {
-  const [markdown, setMarkdown] = useState<string>('# Hello Markdown\nStart typing...')
+  const [markdown, setMarkdown] = useState<string>(
+    '# Hello Markdown\nStart typing...'
+  );
 
   return (
-    <div className="container">
-      <textarea
-        className="editor"
-        value={markdown}
-        onChange={(e) => setMarkdown(e.target.value)}
-      />
-      <div
-        className="preview"
-        dangerouslySetInnerHTML={{ __html: marked(markdown) }}
-      />
+    <div className='container'>
+      <div className='interaction'>
+        <button onClick={() => setMarkdown('')}> Clear All</button>
+      </div>
+      <div className='main'>
+        <textarea
+          className='editor'
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        />
+        <div
+          className='preview'
+          dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
